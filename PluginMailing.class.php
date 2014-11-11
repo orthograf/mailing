bee7faf788cfeda5301e559ae4efa9ab6796d72d
orthograf <orthograf@bk.ru>
2014-11-11 20:18:26 +0700

    Create PluginMailing.class.php

---
+++ PluginMailing.class.php
@@ -0,0 +1,73 @@
+<?php
+
+/* ---------------------------------------------------------------------------
+ * @Plugin Name: Mailing
+ * @Plugin Id: mailing
+ * @Plugin URI:
+ * @Description: Mass mailing for users
+ * @Author: stfalcon-studio
+ * @Author URI: http://stfalcon.com
+ * @LiveStreet Version: 1.0.1
+ * @License: GNU GPL v2, http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * ----------------------------------------------------------------------------
+ */
+
+
+/**
+ * Deny direct access to this file
+ */
+if (!class_exists('Plugin')) {
+    die('Hacking attempt!');
+}
+
+/**
+ * Mailing Plugin class for LiveStreet
+ */
+class PluginMailing extends Plugin
+{
+
+    public $aInherits = array(
+        'mapper' => array(
+            'ModuleTalk_MapperTalk' => '_ModuleTalk_MapperTalk',
+            'ModuleUser_MapperUser' => '_ModuleUser_MapperUser'
+            ),
+        'module' => array(
+            'ModuleUser' => '_ModuleUser',
+        )
+    );
+
+    /**
+     * Plugin Activation
+     *
+     * @return boolean
+     */
+    public function Activate()
+    {
+        $results = $this->ExportSQL(dirname(__FILE__) . '/activate.sql');
+        $this->Cache_Clean();
+        return $results['result'];
+    }
+
+    /**
+     * Деактивация плагина
+     *
+     * @return boolean
+     */
+    public function Deactivate()
+    {
+        $results = $this->ExportSQL(dirname(__FILE__) . '/deactivate.sql');
+        $this->Cache_Clean();
+        return $results['result'];
+    }
+
+    /**
+     * Plugin Initialization
+     *
+     * @return void
+     */
+    public function Init()
+    {
+        $this->Viewer_Assign("sTemplateWebPathPluginMailing", Plugin::GetTemplateWebPath(__CLASS__));
+    }
+
+}
