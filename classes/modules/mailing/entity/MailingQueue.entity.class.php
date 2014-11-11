755cc05fa80b4082b64caf1b8e9d71d1f6981268
orthograf <orthograf@bk.ru>
2014-11-11 20:40:22 +0700

    Create MailingQueue.entity.class.php

---
+++ classes/modules/mailing/entity/MailingQueue.entity.class.php
@@ -0,0 +1,64 @@
+<?php
+
+/* ---------------------------------------------------------------------------
+ * @Plugin Name: Mailing
+ * @Plugin Id: mailing
+ * @Plugin URI:
+ * @Description: Mass mailing for users
+ * @Author: stfalcon-studio
+ * @Author URI: http://stfalcon.com
+ * @LiveStreet Version: 1.0.1
+ * @License: GNU GPL v2, http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * ----------------------------------------------------------------------------
+ */
+
+class PluginMailing_ModuleMailing_EntityMailingQueue extends Entity
+{
+    /**
+     * Getters
+     */
+
+    public function getId()
+    {
+        return $this->_aData['id'];
+    }
+    public function getMailingId()
+    {
+        return $this->_aData['mailing_id'];
+    }
+
+    public function getUserId()
+    {
+        return $this->_aData['user_id'];
+    }
+    
+    public function getSended()
+    {
+        return $this->_aData['sended'];
+    }
+
+
+    /**
+     * Setters
+     */
+    public function setId($data)
+    {
+        $this->_aData['id'] = $data;
+    }
+
+    public function setMailingId($data)
+    {
+        $this->_aData['mailing_id'] = $data;
+    }
+
+    public function setUserId($data)
+    {
+        $this->_aData['user_id'] = $data;
+    }
+    
+    public function setSended($data)
+    {
+        $this->_aData['sended'] = $data;
+    }
+
+}
